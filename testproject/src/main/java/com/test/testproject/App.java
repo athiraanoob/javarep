da2d4d9c25cbe6079fd34a4a049705f986cafb39
athiraanoob <50247736+athiraanoob@users.noreply.github.com>
2021-02-18 20:27:06 -0500

    Update App.java

--- testproject/src/main/java/com/test/testproject/App.java
+++ testproject/src/main/java/com/test/testproject/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("First pom project");
         System.out.println("IT will trigger Automatically");
+        System.out.println("Automatic trigger")
     }
 }
